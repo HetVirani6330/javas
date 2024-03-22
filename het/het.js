a448083916a4d071454681f20dad1278840258f0
HetVirani6330 <152472532+HetVirani6330@users.noreply.github.com>
2024-03-22 17:43:00 +0000

    add

--- het/het.js
+++ het/het.js
@@ -1,1 +1,18 @@
 console.log("het")
+console.log("virani het ")
+let a=1000,b=200,c=30
+
+
+if(a>b){
+    if (a>c) {
+        console.log("a is max")
+    }else{
+        console.log("c is Max")        
+    }
+}else{
+    if (b>c) {
+        console.log("B is Max")
+    } else {
+        console.log("C is Max")
+    }
+}
